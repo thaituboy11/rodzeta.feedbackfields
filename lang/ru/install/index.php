fcdb85e0932214b6a054f582f01292e8b3ec8543
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-05-08 15:12:25 +0300

    fix

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,7 +1,7 @@
 <?php
 
-$MESS["RODZETA_FEEDBACKFIELDS_MODULE_NAME"] = "Дополнительные поля для компонента Форма обратной связи";
-$MESS["RODZETA_FEEDBACKFIELDS_MODULE_DESCRIPTION"] = "Данный модуль позволяет добавлять дополнительные поля для стандартного компонента Форма обратной связи bitrix:main.feedback и сохранять данные форм";
+$MESS["RODZETA_FEEDBACKFIELDS_MODULE_NAME"] = "Форма обратной связи";
+$MESS["RODZETA_FEEDBACKFIELDS_MODULE_DESCRIPTION"] = 'Неограниченное число полей для стандартного компонента БУС "Форма обратной связи". Интеграция с Bitrix24.';
 
 $MESS["RODZETA_REQUIREMENTS_PHP_VERSION"] = "Необходима версия PHP 7 или выше";
 $MESS["RODZETA_REQUIREMENTS_BITRIX_UTF8"] = "Bitrix должен быть установлен в кодировке UTF-8";
