ece2e663f0b2f10fa34d2ac5e3a95fc364ffec61
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-05-08 15:22:32 +0300

    fix

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$MESS["RODZETA_FEEDBACKFIELDS_MODULE_NAME"] = "Форма обратной связи";
+$MESS["RODZETA_FEEDBACKFIELDS_MODULE_NAME"] = "Формы обратной связи";
 $MESS["RODZETA_FEEDBACKFIELDS_MODULE_DESCRIPTION"] = 'Неограниченное число полей для стандартного компонента БУС "Форма обратной связи". Интеграция с Bitrix24.';
 
 $MESS["RODZETA_REQUIREMENTS_PHP_VERSION"] = "Необходима версия PHP 7 или выше";
